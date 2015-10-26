8390de86b5955d3a694a3063f35cab77eee963c8
mara gatchalian <maragatchalian@github.com>
2015-10-26 15:50:48 +0800

    Loops in Templates

--- app/views.py
+++ app/views.py
@@ -3,6 +3,17 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     user = {'nickname': 'Mara'} #fake user
-    return render_template('index.html', title='Home', user=user)
+    posts = [ #Fake array of posts
+        {
+            'author': {'nickname': '@ericjandale'},
+            'body': 'I am a Marine Engineer'
+        },
+        {
+            'author': {'nickname': '@marsgatchalian'},
+            'body': 'I am inlove with a Marine Engineer'
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
